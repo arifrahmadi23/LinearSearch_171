7488d2f8336b1b9415aa0064e9060b8141c922cc
arifrahmadi23 <muhammad231arif@gmail.com>
2023-04-14 10:52:08 +0700

    langkah 5 a found

--- LinearSearch_171/LinearSearch_171.cpp
+++ LinearSearch_171/LinearSearch_171.cpp
@@ -46,4 +46,15 @@ void LinearSearch()
 
 		comparison = 0;
 		for (i = 0; i < n; i++)							// Langkah 2,3 dan 4
-		
+		{
+			comparison++;
+			if (arr[i] == item)							// Lankah 5 a found
+			{
+				cout << "\n" << item << " found at position" << (i + 1) << endl;
+				break;
+			}
+		}
+
+
+	}
+}
